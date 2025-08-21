dc0a8d5a67ce1a6fe940c61cb1626e2c9153cd88
Eun Jeong Oh <136856816+oheunj@users.noreply.github.com>
2025-08-21 15:21:14 -0400

    Delete functions/adaptive_lasso.R

--- functions/adaptive_lasso.R
+++
@@ -1,7 +0,0 @@
-adaptive_lasso = function(X, y, method = NULL, q, nfold){
-  enet.b = cv.glmnet(X, y, alpha = 0.5, nfolds = nfold)
-  abs.enet = abs(coef(enet.b, s = "lambda.min")[-1]) + 1 / nrow(X)
-  weights = 1 / abs.enet
-  alass = cv.glmnet(X, y, alpha = 1, penalty.factor = weights, nfolds = nfold)
-  return(as.vector(coef(alass, s = "lambda.min")))
-}
