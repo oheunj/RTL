250965afe58c53e40f6ca3602969547896c013e5
Eun Jeong Oh <136856816+oheunj@users.noreply.github.com>
2025-08-21 15:21:39 -0400

    Add files via upload

---
+++ functions/adaptive_lasso_with_enet_weights.R
@@ -0,0 +1,7 @@
+adaptive_lasso_with_enet_weights = function(X, y, method = NULL, q, nfold){
+  enet.b = cv.glmnet(X, y, alpha = 0.5, nfolds = nfold)
+  abs.enet = abs(coef(enet.b, s = "lambda.min")[-1]) + 1 / nrow(X)
+  weights = 1 / abs.enet
+  alass = cv.glmnet(X, y, alpha = 1, penalty.factor = weights, nfolds = nfold)
+  return(as.vector(coef(alass, s = "lambda.min")))
+}
